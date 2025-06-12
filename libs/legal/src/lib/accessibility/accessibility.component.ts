666374cf5e156851409b845cb7c69c8cc40345e5
Waog <oliver.stadie@gmail.com>
2025-06-12 15:44:45 +0200

    feat(frontend): ✨ accessibility language toggle

--- libs/legal/src/lib/accessibility/accessibility.component.ts
+++ libs/legal/src/lib/accessibility/accessibility.component.ts
@@ -2,11 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+import { Language } from '../language-switch/language.enum';
+import { LanguageService } from '../language-switch/language.service';
+import { LanguageSwitchComponent } from '../language-switch/language-switch.component';
+
 @Component({
   selector: 'legal-accessibility',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, LanguageSwitchComponent],
   templateUrl: './accessibility.component.html',
   styleUrls: ['./accessibility.component.scss'],
 })
-export class AccessibilityComponent {}
+export class AccessibilityComponent {
+  readonly Language = Language;
+
+  constructor(readonly languageService: LanguageService) {}
+}
