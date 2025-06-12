1dbe113b55d889cdbf265497890007eaa3cfaf35
Waog <oliver.stadie@gmail.com>
2025-06-12 15:52:05 +0200

    feat(frontend): ✨ ToS language toggle

--- libs/legal/src/lib/terms-of-service/terms-of-service.component.ts
+++ libs/legal/src/lib/terms-of-service/terms-of-service.component.ts
@@ -2,11 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+import { Language } from '../language-switch/language.enum';
+import { LanguageService } from '../language-switch/language.service';
+import { LanguageSwitchComponent } from '../language-switch/language-switch.component';
+
 @Component({
   selector: 'legal-terms-of-service',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, LanguageSwitchComponent],
   templateUrl: './terms-of-service.component.html',
   styleUrls: ['./terms-of-service.component.scss'],
 })
-export class TermsOfServiceComponent {}
+export class TermsOfServiceComponent {
+  readonly Language = Language;
+
+  constructor(readonly languageService: LanguageService) {}
+}
