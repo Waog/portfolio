3d65e85c3c643c2b0503e9bfcf7686aca3847545
Waog <oliver.stadie@gmail.com>
2025-06-12 17:02:20 +0200

    fix(frontend): 🗑️ remove unnecessary legal documents

--- libs/legal/src/index.ts
+++ libs/legal/src/index.ts
@@ -1,7 +1,4 @@
-export * from './lib/accessibility/accessibility.component';
 export * from './lib/cookie-policy/cookie-policy.component';
-export * from './lib/disclaimer/disclaimer.component';
 export * from './lib/imprint/imprint.component';
 export * from './lib/lib.routes';
 export * from './lib/privacy-policy/privacy-policy.component';
-export * from './lib/terms-of-service/terms-of-service.component';
--- libs/legal/src/lib/lib.routes.ts
+++ libs/legal/src/lib/lib.routes.ts
@@ -1,18 +1,12 @@
 import { Route } from '@angular/router';
 
-import { AccessibilityComponent } from './accessibility/accessibility.component';
 import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
-import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
 export const legalRoutes: Route[] = [
   { path: 'imprint', component: ImprintComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
-  { path: 'terms-of-service', component: TermsOfServiceComponent },
-  { path: 'accessibility', component: AccessibilityComponent },
   { path: 'cookie-policy', component: CookiePolicyComponent },
-  { path: 'disclaimer', component: DisclaimerComponent },
   { path: '', redirectTo: 'imprint', pathMatch: 'full' },
 ];
