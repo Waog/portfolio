3b7796931e34b11c006084c507fa2b8d659272c2
Waog <oliver.stadie@gmail.com>
2025-06-12 15:49:39 +0200

    feat(frontend): ✨ privacy policy language toggle

--- libs/legal/src/lib/privacy-policy/privacy-policy.component.ts
+++ libs/legal/src/lib/privacy-policy/privacy-policy.component.ts
@@ -2,11 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+import { Language } from '../language-switch/language.enum';
+import { LanguageService } from '../language-switch/language.service';
+import { LanguageSwitchComponent } from '../language-switch/language-switch.component';
+
 @Component({
   selector: 'legal-privacy-policy',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, LanguageSwitchComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrls: ['./privacy-policy.component.scss'],
 })
-export class PrivacyPolicyComponent {}
+export class PrivacyPolicyComponent {
+  readonly Language = Language;
+
+  constructor(readonly languageService: LanguageService) {}
+}
