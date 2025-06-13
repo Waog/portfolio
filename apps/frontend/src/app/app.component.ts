8e534b697111a868f63b04f50bf9aafb2919846a
Waog <oliver.stadie@gmail.com>
2025-06-13 20:09:01 +0200

    feat(frontend): ✨ add nav bar

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from '@portfolio/shared-ui';
+import { FooterComponent, NavigationComponent } from '@portfolio/shared-ui';
 
 @Component({
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, FooterComponent, NavigationComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
