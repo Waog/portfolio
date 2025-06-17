8109b7a45928597770fedda71c7444be175e72ec
Waog <oliver.stadie@gmail.com>
2025-06-17 14:13:12 +0200

    test(frontend): ✨ fix ProjectItem unit test

--- libs/projects/src/lib/project-item/project-item.component.spec.ts
+++ libs/projects/src/lib/project-item/project-item.component.spec.ts
@@ -1,10 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Project } from '../models/project';
 import { ProjectItemComponent } from './project-item.component';
 
 describe('ProjectItemComponent', () => {
   let component: ProjectItemComponent;
   let fixture: ComponentFixture<ProjectItemComponent>;
+
+  const mockProject: Project = {
+    id: 'test-project',
+    title: 'Test Project',
+    projectType: 'Web Application',
+    compactDescription: 'A test project for unit testing',
+    keyAchievements: 'Successfully implemented testing',
+    fullDescription: 'This is a full description of the test project',
+    features: ['Feature 1', 'Feature 2'],
+    highlights: ['Highlight 1', 'Highlight 2'],
+    technologies: ['Angular', 'TypeScript'],
+    role: 'Developer',
+    team: 'Development Team',
+    fromTo: '2024-01-01 to 2024-12-31',
+    duration: '12 months',
+    location: 'Remote',
+    workMode: 'Remote',
+    company: 'Test Company',
+    industry: 'Technology',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +34,9 @@ describe('ProjectItemComponent', () => {
 
     fixture = TestBed.createComponent(ProjectItemComponent);
     component = fixture.componentInstance;
+
+    // Set the required input before detectChanges
+    fixture.componentRef.setInput('project', mockProject);
     fixture.detectChanges();
   });
 
