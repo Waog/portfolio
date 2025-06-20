0961e31eeee15917da9b5dc2c0eaec465226c5c9
Waog <oliver.stadie@gmail.com>
2025-06-20 22:21:58 +0200

    chore(frontend): ✨ add shared storybook config & fill `about-me` storybook

--- apps/frontend/.storybook/main.ts
+++ apps/frontend/.storybook/main.ts
@@ -1,12 +1,20 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: [
+    '../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+    '../../../libs/**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/angular',
-    options: {},
+    options: {
+      builder: {
+        viteConfigPath: 'vite.config.mts',
+      },
+    },
   },
+  staticDirs: ['../public'],
   previewHead: head => `
     ${head}
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -15,6 +23,6 @@ const config: StorybookConfig = {
 
 export default config;
 
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
 // and https://nx.dev/recipes/storybook/custom-builder-configs
