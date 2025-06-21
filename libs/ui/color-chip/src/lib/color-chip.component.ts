6702f0b700d492a8eb41e6d9d26e751a1d5863aa
Waog <oliver.stadie@gmail.com>
2025-06-21 19:58:38 +0200

    feat(frontend): ✨ add color-chip lib (empty)

---
+++ libs/ui/color-chip/src/lib/color-chip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-color-chip',
+  imports: [CommonModule],
+  templateUrl: './color-chip.component.html',
+  styleUrl: './color-chip.component.scss',
+})
+export class ColorChipComponent {}
