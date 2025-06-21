b27af30f8a9c00bd0785b5462de9917b925c3fc7
Waog <oliver.stadie@gmail.com>
2025-06-21 08:28:38 +0200

    refactor(frontend): ♻️ `nx g move` `about-me` lib to `libs/feature` folder

--- libs/feature/about-me/jest.config.ts
+++ libs/feature/about-me/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'about-me',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/about-me',
+  coverageDirectory: '../../../coverage/libs/feature/about-me',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
