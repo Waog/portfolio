43a8c76cdb0e39f8db731e527edeec0c3d3a22c2
Waog <oliver.stadie@gmail.com>
2025-06-23 13:39:30 +0200

    feat(frontend): 🚧 generate nx lib `feature/skill-section`

---
+++ libs/feature/skill-section/src/lib/skill-section.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-skill-section',
+  imports: [CommonModule],
+  templateUrl: './skill-section.component.html',
+  styleUrl: './skill-section.component.scss',
+})
+export class SkillSectionComponent {}
