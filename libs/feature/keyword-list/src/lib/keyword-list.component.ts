d3ae889162b7d3579a322eedcc6087a2bef5b758
Waog <oliver.stadie@gmail.com>
2025-06-23 11:34:58 +0200

    feat(frontend): 🚧 generate nx lib `feature/keyword-list`

---
+++ libs/feature/keyword-list/src/lib/keyword-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-keyword-list',
+  imports: [CommonModule],
+  templateUrl: './keyword-list.component.html',
+  styleUrl: './keyword-list.component.scss',
+})
+export class KeywordListComponent {}
