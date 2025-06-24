251ae4689a4c26ff21f356cf9f1d17a298fc3b4c
Waog <oliver.stadie@gmail.com>
2025-06-24 20:48:35 +0200

    feat(frontend): 🚧 generate nx lib `feature/contact-section`

---
+++ libs/feature/contact-section/src/lib/contact-section.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-contact-section',
+  imports: [CommonModule],
+  templateUrl: './contact-section.component.html',
+  styleUrl: './contact-section.component.scss',
+})
+export class ContactSectionComponent {}
