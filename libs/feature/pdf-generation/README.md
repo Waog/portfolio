56b083488d47e1db93037eb411a817aa778862e9
Waog <oliver.stadie@gmail.com>
2025-06-27 12:44:28 +0200

    feat(frontend): 🚧 generate nx lib `feature/pdf-generation`

---
+++ libs/feature/pdf-generation/README.md
@@ -0,0 +1,7 @@
+# pdf-generation
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test pdf-generation` to execute the unit tests.
---
+++ libs/feature/pdf-generation/src/lib/pdf-generation.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-pdf-generation',
+  imports: [CommonModule],
+  templateUrl: './pdf-generation.component.html',
+  styleUrl: './pdf-generation.component.scss',
+})
+export class PdfGenerationComponent {}
