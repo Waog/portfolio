25c9d950d628d32cd6603ec79bbdb98c42d285bf
Waog <oliver.stadie@gmail.com>
2025-07-08 23:21:16 +0200

    feat(frontend): 🚧 generate nx lib `ui/section`

---
+++ libs/ui/section/src/lib/section.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-section',
+  imports: [CommonModule],
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.scss',
+})
+export class SectionComponent {}
