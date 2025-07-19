187613cc42488e92ef1cebee427c6e2b57a3a7b5
Waog <oliver.stadie@gmail.com>
2025-07-19 17:34:27 +0200

    feat(frontend): ✨ add taxonomy data and unit-tests

---
+++ libs/data-access/taxonomy/src/lib/taxonomy.data.spec.ts
@@ -0,0 +1,128 @@
+import 'jest-expect-message';
+
+import { TagName, TAXONOMY, TaxonomyTerm } from './taxonomy.data';
+
+describe('Taxonomy Data', () => {
+  it('has ordered elements (by canonical name)', () => {
+    const canonicalNames = TAXONOMY.map(term => term.canonical);
+
+    expect(canonicalNames).toEqual([...canonicalNames].sort());
+  });
+
+  it('`includes` elements exist as another TAXONOMY element', () => {
+    throwIfBrokenReference('includes');
+  });
+
+  it('`related` elements exist as another TAXONOMY element', () => {
+    throwIfBrokenReference('related');
+  });
+
+  it('`parents` elements exist as another TAXONOMY element', () => {
+    throwIfBrokenReference('parents');
+  });
+
+  it('`children` elements exist as another TAXONOMY element', () => {
+    throwIfBrokenReference('children');
+  });
+
+  it('`children` elements have a `parents` counterpart', () => {
+    throwIfBrokenReference('children', 'parents');
+  });
+
+  it('`parents` elements have a `children` counterpart', () => {
+    throwIfBrokenReference('parents', 'children');
+  });
+
+  it('array properties are ordered alphabetically', () => {
+    const arrayProperties: (keyof TaxonomyTerm)[] = [
+      'synonyms',
+      'includes',
+      'related',
+      'parents',
+      'children',
+    ];
+
+    for (const term of TAXONOMY) {
+      for (const propName of arrayProperties) {
+        const propValue = term[propName];
+
+        if (!propValue || !Array.isArray(propValue)) continue;
+
+        expect(
+          propValue,
+          `Taxonomy Element "${term.canonical}" has property "${propName}" that is not ordered alphabetically`
+        ).toEqual([...propValue].sort(customSort));
+      }
+    }
+  });
+
+  function customSort(
+    a: RegExp | string | TagName,
+    b: RegExp | string | TagName
+  ): number {
+    const aString = a instanceof RegExp ? a.source : String(a);
+    const bString = b instanceof RegExp ? b.source : String(b);
+    return aString.localeCompare(bString);
+  }
+
+  function throwIfBrokenReference(
+    sourcePropertyName: keyof TaxonomyTerm,
+    targetPropertyName: keyof TaxonomyTerm | undefined = undefined
+  ) {
+    for (const sourceTerm of TAXONOMY) {
+      const sourceValues: TagName[] = sourceTerm[
+        sourcePropertyName
+      ] as TagName[];
+
+      if (!sourceValues) continue;
+
+      for (const sourceValue of sourceValues) {
+        const targetTerm = TAXONOMY.find(
+          targetTerm => targetTerm.canonical === sourceValue
+        );
+
+        expect(
+          targetTerm,
+          `Taxonomy Element "${sourceTerm.canonical}" has property "${sourcePropertyName}" with element "${sourceValue}", but "${sourceValue}" is not defined as a canonical term in TAXONOMY`
+        ).toBeDefined();
+
+        if (!targetPropertyName) continue; // only check for canonical name if no target property is specified
+
+        const targetValue = targetTerm![targetPropertyName];
+
+        if (typeof targetValue === 'string') {
+          const contextMsg = `Taxonomy Element "${
+            sourceTerm.canonical
+          }" has property "${sourcePropertyName}" with element "${sourceValue}", but Taxonomy Element "${
+            targetTerm!.canonical
+          }"'s property "${targetPropertyName}" !== "${
+            sourceTerm.canonical
+          }" (but is "${targetValue}")`;
+
+          expect(targetValue, contextMsg).toBe(sourceTerm.canonical);
+        } else if (Array.isArray(targetValue)) {
+          const contextMsg = `Taxonomy Element "${
+            sourceTerm.canonical
+          }" has property "${sourcePropertyName}" with element "${sourceValue}", but Taxonomy Element "${
+            targetTerm!.canonical
+          }"'s property "${targetPropertyName}" does not include "${
+            sourceTerm.canonical
+          }"`;
+
+          expect(targetValue, contextMsg).toContain(sourceTerm.canonical);
+        } else {
+          const contextMsg = `Taxonomy Element "${
+            sourceTerm.canonical
+          }" has property "${sourcePropertyName}" with element "${sourceValue}", but Taxonomy Element "${
+            targetTerm!.canonical
+          }"'s doesn't have a property "${targetPropertyName}" of type string or string[]`;
+
+          expect(
+            typeof targetValue === 'string' || Array.isArray(targetValue),
+            contextMsg
+          ).toBeTruthy();
+        }
+      }
+    }
+  }
+});
---
+++ libs/data-access/taxonomy/src/lib/taxonomy.data.ts
@@ -0,0 +1,111 @@
+export type TagName =
+  | 'Angular'
+  | 'AngularJS'
+  | 'Angular Material'
+  | 'CSS'
+  | 'Frontend Framework'
+  | 'HTML'
+  | 'JavaScript'
+  | 'React'
+  | 'React Native'
+  | 'React Web'
+  | 'RxJS'
+  | 'SASS'
+  | 'SCSS'
+  | 'TypeScript'
+  | 'Vue.js';
+
+export type TaxonomyTerm = {
+  canonical: TagName;
+  synonyms?: (RegExp | string)[];
+  includes?: TagName[];
+  related?: TagName[];
+  parents?: TagName[];
+  children?: TagName[];
+};
+
+export const TAXONOMY: TaxonomyTerm[] = [
+  {
+    canonical: 'Angular',
+    synonyms: [/angular/i],
+    includes: ['CSS', 'HTML', 'JavaScript'],
+    related: ['Angular Material', 'AngularJS', 'RxJS', 'SASS', 'SCSS'],
+    parents: ['Frontend Framework'],
+    children: ['Angular Material'],
+  },
+  {
+    canonical: 'Angular Material',
+    synonyms: [/angular material/i],
+    parents: ['Angular'],
+  },
+  {
+    canonical: 'AngularJS',
+    synonyms: [/angular\.js/i, /angularjs/i],
+    parents: ['Frontend Framework'],
+    includes: ['CSS', 'HTML', 'TypeScript'],
+    related: ['Angular'],
+  },
+  {
+    canonical: 'CSS',
+    synonyms: [/^css$/i],
+  },
+  {
+    canonical: 'Frontend Framework',
+    synonyms: [/frontend framework/i, /javascript framework/i, /js framework/i],
+    children: ['Angular', 'AngularJS', 'React', 'Vue.js'],
+  },
+  {
+    canonical: 'HTML',
+    synonyms: [/html/i],
+  },
+  {
+    canonical: 'JavaScript',
+    synonyms: [/^js$/i, /javascript/i],
+    children: ['TypeScript'],
+  },
+  {
+    canonical: 'React',
+    synonyms: [/react/i],
+    parents: ['Frontend Framework'],
+    children: ['React Native', 'React Web'],
+    includes: ['CSS', 'HTML', 'TypeScript'],
+  },
+  {
+    canonical: 'React Native',
+    synonyms: [/react native/i],
+    parents: ['React'],
+  },
+  {
+    canonical: 'React Web',
+    synonyms: [/react web/i],
+    parents: ['React'],
+  },
+  {
+    canonical: 'RxJS',
+    synonyms: [/rxjs/i],
+    related: ['Angular'],
+    includes: ['TypeScript'],
+  },
+  {
+    canonical: 'SASS',
+    synonyms: [/sass/i],
+    related: ['SCSS'],
+    includes: ['CSS'],
+  },
+  {
+    canonical: 'SCSS',
+    synonyms: [/scss/i],
+    related: ['SASS'],
+    includes: ['CSS'],
+  },
+  {
+    canonical: 'TypeScript',
+    synonyms: [/^ts$/i, /typescript/i],
+    parents: ['JavaScript'],
+  },
+  {
+    canonical: 'Vue.js',
+    synonyms: [/vue/i, /vue\.js/i, /vuejs/i],
+    parents: ['Frontend Framework'],
+  },
+];
