0bdcb4442c49a3e41c3b3425c00e771814263fb6
Waog <oliver.stadie@gmail.com>
2025-07-25 11:46:56 +0200

    fix(frontend): ✨ generate `memoize` nx lib

---
+++ libs/util/memoize/src/lib/memoize.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-memoize',
+  imports: [CommonModule],
+  templateUrl: './memoize.component.html',
+  styleUrl: './memoize.component.scss',
+})
+export class MemoizeComponent {}
