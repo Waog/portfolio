87937a2e423761ed04ce4c03d9fb033075981f56
Waog <oliver.stadie@gmail.com>
2025-09-23 23:41:38 +0200

    feat(frontend): ✨ add 'Generali Heroes' project entry

--- apps/frontend-e2e/src/project-list.spec.ts
+++ apps/frontend-e2e/src/project-list.spec.ts
@@ -8,7 +8,7 @@ test.describe('Project List Section', () => {
 
     const projectList = await getProjectListSection(page);
 
-    await expect(projectList.locator('lib-project-item')).toHaveCount(23);
+    await expect(projectList.locator('lib-project-item')).toHaveCount(24);
 
     await expect(
       page.getByRole('heading', { name: 'Top Matching Projects' })
