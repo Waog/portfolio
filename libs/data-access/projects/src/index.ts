bcf25bbbd066576510da7273707e9fc0dfe0d5a4
Waog <oliver.stadie@gmail.com>
2026-03-04 12:26:39 +0100

    refactor(frontend): 🗑️ delete obsolete unused code
    - after web worker search engine is in place

--- libs/data-access/projects/src/index.ts
+++ libs/data-access/projects/src/index.ts
@@ -1,6 +1,4 @@
 export * from './lib/project';
-export * from './lib/project.service';
 export * from './lib/projects-factory';
 export * from './lib/skills-without-projects';
 export * from './lib/technology-matcher';
-export * from './lib/technology-matching.service';
