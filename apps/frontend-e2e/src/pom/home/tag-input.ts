ff3124b2e0f96501901a0a07328a64ef5f3f4f60
Waog <oliver.stadie@gmail.com>
2026-03-17 19:57:43 +0100

    test(frontend): 📦 e2e tests: tagInput section - uses POM
    - tests the tagInput section, including URL and chips
    - split and removed some tests

--- apps/frontend-e2e/src/pom/home/tag-input.ts
+++ apps/frontend-e2e/src/pom/home/tag-input.ts
@@ -23,11 +23,31 @@ export class TagInput {
     this.spinner = this.tagList.locator('.progress-indicator');
   }
 
-  async addSearchTerm(term: string) {
+  async addSearchTerm(
+    term: string,
+    options: {
+      skipAssertions?: boolean;
+      submitMethod?: 'EnterKey' | 'AddButton';
+    } = {}
+  ) {
     await this.inputField.fill(term);
-    await this.addButton.click();
-    await expect(this.chipTexts.last()).toHaveText(term);
-    await expect(this.spinner).toBeVisible();
-    await expect(this.spinner).toBeHidden();
+    if (options?.submitMethod === 'EnterKey') {
+      await this.inputField.press('Enter');
+    } else {
+      await this.addButton.click();
+    }
+    if (!options?.skipAssertions) {
+      await expect(this.chipTexts.last()).toHaveText(term);
+      await expect(this.spinner).toBeVisible();
+      await expect(this.spinner).toBeHidden();
+    }
+  }
+
+  async removeSearchTerm(term: string) {
+    const chip = this.chips.filter({ hasText: term });
+    await expect(chip).toHaveCount(1);
+    const closeButton = chip.locator('button.chip-close-button');
+    await closeButton.click();
+    await expect(chip).toHaveCount(0);
   }
 }
