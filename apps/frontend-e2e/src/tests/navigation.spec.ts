5f5fc99de4bfe479b6e3da2bacf5dbd5479e0be2
Waog <oliver.stadie@gmail.com>
2026-03-17 21:38:27 +0100

    test(frontend): 📦 add e2e tests: navigation - url hashes scroll to section

--- apps/frontend-e2e/src/tests/navigation.spec.ts
+++ apps/frontend-e2e/src/tests/navigation.spec.ts
@@ -5,53 +5,33 @@ test.describe('Navigation', () => {
     await expect(navigation.locator).toBeVisible();
   });
 
-  test('navigates to Projects and updates URL', async ({
-    homePage,
-    navigation,
-    urlHelper,
-  }) => {
+  test('Projects Item updates URL', async ({ navigation, urlHelper }) => {
     await navigation.getNavItem('Projects').click();
-    await expect(homePage.locator).toBeVisible();
     expect(urlHelper.isHomePage()).toBe(true);
     expect(urlHelper.hasProjectsFragment()).toBe(true);
   });
 
-  test('navigates to Skills and updates URL', async ({
-    homePage,
-    navigation,
-    urlHelper,
-  }) => {
+  test('Skills Item updates URL', async ({ navigation, urlHelper }) => {
     await navigation.getNavItem('Skills').click();
-    await expect(homePage.locator).toBeVisible();
     expect(urlHelper.isHomePage()).toBe(true);
     expect(urlHelper.hasSkillsFragment()).toBe(true);
   });
 
-  test('navigates to About Me and updates URL', async ({
-    homePage,
-    navigation,
-    urlHelper,
-  }) => {
+  test('About Me Item updates URL', async ({ navigation, urlHelper }) => {
     await navigation.getNavItem('About me').click();
-    await expect(homePage.locator).toBeVisible();
     expect(urlHelper.isHomePage()).toBe(true);
     expect(urlHelper.hasAboutMeFragment()).toBe(true);
   });
 
-  test('navigates to Contact and updates URL', async ({
-    homePage,
-    navigation,
-    urlHelper,
-  }) => {
+  test('Contact Item updates URL', async ({ navigation, urlHelper }) => {
     await navigation.getNavItem('Contact').click();
-    await expect(homePage.locator).toBeVisible();
     expect(urlHelper.isHomePage()).toBe(true);
     expect(urlHelper.hasContactFragment()).toBe(true);
   });
 });
 
-test.describe('Viewport Checks', () => {
-  test('checks if Projects section is in the viewport', async ({
+test.describe('Navigation: Viewport Checks', () => {
+  test('selecting Projects item scrolls projects section into viewport', async ({
     homePage,
     navigation,
   }) => {
@@ -59,7 +39,7 @@ test.describe('Viewport Checks', () => {
     await expect(homePage.projectList().locator).toBeInViewport();
   });
 
-  test('checks if Skills section is in the viewport', async ({
+  test('selecting Skills item scrolls skills section into viewport', async ({
     homePage,
     navigation,
   }) => {
@@ -67,7 +47,7 @@ test.describe('Viewport Checks', () => {
     await expect(homePage.skills().locator).toBeInViewport();
   });
 
-  test('checks if About Me section is in the viewport', async ({
+  test('selecting About Me item scrolls About Me section into viewport', async ({
     homePage,
     navigation,
   }) => {
@@ -75,11 +55,43 @@ test.describe('Viewport Checks', () => {
     await expect(homePage.aboutMe().locator).toBeInViewport();
   });
 
-  test('checks if Contact section is in the viewport', async ({
+  test('selecting Contact item scrolls Contact section into viewport', async ({
     homePage,
     navigation,
   }) => {
     await navigation.getNavItem('Contact').click();
+    await expect(homePage.contactMe().locator).toBeInViewport();
+  });
+
+  test('loading page with #projects fragment scrolls projects section into viewport', async ({
+    urlHelper,
+    homePage,
+  }) => {
+    await urlHelper.gotoHomePage({ fragment: 'projects' });
+    await expect(homePage.projectList().locator).toBeInViewport();
+  });
+
+  test('loading page with #skills fragment scrolls skills section into viewport', async ({
+    urlHelper,
+    homePage,
+  }) => {
+    await urlHelper.gotoHomePage({ fragment: 'skills' });
+    await expect(homePage.skills().locator).toBeInViewport();
+  });
+
+  test('loading page with #about-me fragment scrolls About Me section into viewport', async ({
+    urlHelper,
+    homePage,
+  }) => {
+    await urlHelper.gotoHomePage({ fragment: 'about-me' });
+    await expect(homePage.aboutMe().locator).toBeInViewport();
+  });
+
+  test('loading page with #contact fragment scrolls Contact section into viewport', async ({
+    urlHelper,
+    homePage,
+  }) => {
+    await urlHelper.gotoHomePage({ fragment: 'contact' });
     await expect(homePage.contactMe().locator).toBeInViewport();
   });
 });
