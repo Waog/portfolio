2eed8ecb0fb7773ffa1eba9cf172478c1b5a5958
Waog <oliver.stadie@gmail.com>
2026-03-17 14:49:30 +0100

    test(frontend): 📦 e2e tests: skill section - uses POM
    - added a test case to check sorting

--- apps/frontend-e2e/src/tests/skill-section.spec.ts
+++ apps/frontend-e2e/src/tests/skill-section.spec.ts
@@ -1,36 +1,49 @@
-import { expect, type Locator, type Page, test } from '@playwright/test';
+import { expect, test } from '../fixtures/app.fixture';
 
 test.describe('Skills Section', () => {
-  test('displays some categories', async ({ page }) => {
-    await page.goto('/');
-
-    const skillsSection = await getSkillsSection(page);
-    await expect(skillsSection.getByText(/^Frontend:$/)).toBeVisible();
-    await expect(skillsSection.getByText(/^Backend:$/)).toBeVisible();
-    await expect(skillsSection.getByText(/^Misc:$/)).toBeVisible();
+  test('is visible', async ({ homePage }) => {
+    const skills = homePage.skills();
+    await expect(skills.locator).toBeVisible();
   });
 
-  test('displays frontend technologies', async ({ page }) => {
-    await page.goto('/?searchTags=Angular,React');
+  test('displays some categories', async ({ homePage }) => {
+    const skills = homePage.skills();
 
-    const skillsSection = await getSkillsSection(page);
-
-    await expect(skillsSection.getByText('Angular').first()).toBeVisible();
-    await expect(skillsSection.getByText('React Native').first()).toBeVisible();
+    await expect(await skills.skillCategory(/^Frontend:$/)).toBeVisible();
+    await expect(await skills.skillCategory(/^Backend:$/)).toBeVisible();
+    await expect(await skills.skillCategory(/^Misc:$/)).toBeVisible();
   });
 
-  test('displays backend technologies', async ({ page }) => {
-    await page.goto('/?searchTags=Node,Java');
+  test('categories are sorted by search terms', async ({
+    homePage,
+    urlHelper,
+  }) => {
+    await urlHelper.gotoHomePage({
+      searchTags: ['GitHub Actions', 'Travis CI', 'Spring Boot'],
+    });
+    const skills = homePage.skills();
 
-    const skillsSection = await getSkillsSection(page);
+    await expect(skills.skillCategories.nth(0)).toContainText(/DevOps/);
+    await expect(skills.skillCategories.nth(1)).toContainText(/Backend/);
+  });
 
-    await expect(skillsSection.getByText('Node.js').first()).toBeVisible();
-    await expect(skillsSection.getByText('Java').first()).toBeVisible();
+  test('displays frontend technologies', async ({ homePage, urlHelper }) => {
+    // NOTE: use search tags, to ensure the skill tags are displayed and not in collapsed list.
+    await urlHelper.gotoHomePage({ searchTags: ['Angular', 'React Native'] });
+
+    const skills = homePage.skills();
+
+    await expect(skills.tagsBy(/Angular/).first()).toBeVisible();
+    await expect(skills.tagsBy(/React Native/).first()).toBeVisible();
+  });
+
+  test('displays backend technologies', async ({ homePage, urlHelper }) => {
+    // NOTE: use search tags, to ensure the skill tags are displayed and not in collapsed list.
+    await urlHelper.gotoHomePage({ searchTags: ['Java', 'Node.js'] });
+
+    const skills = homePage.skills();
+
+    await expect(skills.tagsBy(/Java/).first()).toBeVisible();
+    await expect(skills.tagsBy(/Node.js/).first()).toBeVisible();
   });
 });
-
-async function getSkillsSection(page: Page): Promise<Locator> {
-  const skillsSection = page.getByText(/Skills.*Backend.*more/s);
-  await expect(skillsSection).toBeVisible();
-  return skillsSection;
-}
