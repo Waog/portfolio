f01a2937915171ed36fa29e9ce3110f5482ff4ce
Waog <oliver.stadie@gmail.com>
2026-04-02 14:02:55 +0200

    feat(frontend): ✨ add Top Loading Bar below navigation
    - to represent the global loading state

--- apps/frontend-e2e/src/pom/navigation/navigation.ts
+++ apps/frontend-e2e/src/pom/navigation/navigation.ts
@@ -4,14 +4,24 @@ export class Navigation {
   readonly locator: Locator;
   readonly brandLink: Locator;
   readonly navItems: Locator;
+  readonly progressBar: Locator;
 
   constructor(private page: Page) {
     this.locator = page.locator('lib-navigation mat-toolbar');
     this.brandLink = this.locator.locator('a.brand-link');
     this.navItems = this.locator.locator('a.nav-item');
+    this.progressBar = this.locator.locator(
+      'mat-progress-bar.top-loading-progress-bar'
+    );
   }
 
   getNavItem(name: string): Locator {
     return this.navItems.getByText(name);
+  }
+
+  getProgressBarBetween(min: number, max: number): Locator {
+    return this.locator.locator(
+      `xpath=.//mat-progress-bar[contains(@class, 'top-loading-progress-bar') and number(@aria-valuenow) >= ${min} and number(@aria-valuenow) <= ${max}]`
+    );
   }
 }
