cef5e9379b6822e1eba912f8dfdbfe177016e2e4
Waog <oliver.stadie@gmail.com>
2026-05-01 02:18:44 +0200

    ci(root): 📦 commitlint ignores GitHub Copilot Cloud Agent commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,9 @@ module.exports = {
   ignores: [
     // Ignore dependabot commit messages
     message => message.includes('Signed-off-by: dependabot[bot]'),
+
+    // Ignore commit messages by GitHub Copilot cloud agent
+    message => message.includes('Agent-Logs-Url: https://github.com/'),
   ],
   defaultIgnores: true, // Ensures default ignores (like 'Merge branch...') are kept
 };
